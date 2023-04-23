3aa3ab0c7d454c545c51744c1e056696c4f5dd8c
Justinas Kavoliūnas <justinaskav@gmail.com>
2023-04-23 19:09:35 +0300

    Make the same URL as old report

--- ataskaita2022/data/bendruomene.EN.ts
+++ ataskaita2022/data/bendruomene.EN.ts
@@ -1,76 +1,76 @@
 export const teamPhotos = [
     {
-        link: '/ataskaita2022/img/koordinatoriu-nuotraukos/bendros/Centrinis-biuras-2.jpg',
+        link: '/ataskaita-2022/img/koordinatoriu-nuotraukos/bendros/Centrinis-biuras-2.jpg',
         title: 'Central Office',
         href: '#central-office',
     },
     {
-        link: '/ataskaita2022/img/koordinatoriu-nuotraukos/bendros/Taryba-2.jpg',
+        link: '/ataskaita-2022/img/koordinatoriu-nuotraukos/bendros/Taryba-2.jpg',
         title: 'VU SR Board',
         href: '/en/vu-sa/taryba',
     },
     {
-        link: '/ataskaita2022/img/kitos-nuotraukos/Parlamentas.jpg',
+        link: '/ataskaita-2022/img/kitos-nuotraukos/Parlamentas.jpg',
         title: 'VU SR Parliament',
         href: '/en/vu-sa/parlamentas',
     },
     {
-        link: '/ataskaita2022/img/kitos-nuotraukos/ISF.jpg',
+        link: '/ataskaita-2022/img/kitos-nuotraukos/ISF.jpg',
         title: 'Institutional Strengthening Fund',
         href: '/en/stipri-organizacija/isf'
     },
     {
-        link: '/ataskaita2022/img/koordinatoriu-nuotraukos/bendros/Socialinio-akademinio-proceso-koordinatoriai-2.jpg',
+        link: '/ataskaita-2022/img/koordinatoriu-nuotraukos/bendros/Socialinio-akademinio-proceso-koordinatoriai-2.jpg',
         title: 'Coordinators of Socioacademic Affairs',
         href: '#coordinators-of-socioacademic-affairs'
     },
     {
-        link: '/ataskaita2022/img/koordinatoriu-nuotraukos/bendros/atstovu.jpg',
+        link: '/ataskaita-2022/img/koordinatoriu-nuotraukos/bendros/atstovu.jpg',
         title: 'Representatives\' Coordinators',
         href: '#representatives-coordinators'
     },
     {
-        link: '/ataskaita2022/img/koordinatoriu-nuotraukos/bendros/Komunikacijos-koordinatoriai-2.jpg',
+        link: '/ataskaita-2022/img/koordinatoriu-nuotraukos/bendros/Komunikacijos-koordinatoriai-2.jpg',
         title: 'Coordinators of Communication',
         href: '#coordinators-of-communication'
     },
     {
-        link: '/ataskaita2022/img/koordinatoriu-nuotraukos/bendros/Marketingo-srities-koordinatoriai-2.jpg',
+        link: '/ataskaita-2022/img/koordinatoriu-nuotraukos/bendros/Marketingo-srities-koordinatoriai-2.jpg',
         title: 'Coordinators of Marketing',
         href: '#coordinators-of-marketing'
     },
     {
-        link: '/ataskaita2022/img/koordinatoriu-nuotraukos/bendros/organizacines.jpg',
+        link: '/ataskaita-2022/img/koordinatoriu-nuotraukos/bendros/organizacines.jpg',
         title: 'Coordinators of Organizational Affairs',
         href: '#coordinators-of-organizational-affairs'
     },
     {
-        link: '/ataskaita2022/img/koordinatoriu-nuotraukos/bendros/HR-2.jpg',
+        link: '/ataskaita-2022/img/koordinatoriu-nuotraukos/bendros/HR-2.jpg',
         title: 'Coordinators of Human Resources',
         href: '#coordinators-of-human-resources'
     },
     {
-        link: '/ataskaita2022/img/koordinatoriu-nuotraukos/bendros/Integracijos-proceso-koordinatoriai-2.jpg',
+        link: '/ataskaita-2022/img/koordinatoriu-nuotraukos/bendros/Integracijos-proceso-koordinatoriai-2.jpg',
         title: 'Coordinators of Integration Process',
         href: '#coordinators-of-integration-process'
     },
     {
-        link: '/ataskaita2022/img/koordinatoriu-nuotraukos/bendros/Administratoriai-2.jpg',
+        link: '/ataskaita-2022/img/koordinatoriu-nuotraukos/bendros/Administratoriai-2.jpg',
         title: 'Administrators',
         href: '#administrators'
     },
     {
-        link: '/ataskaita2022/img/koordinatoriu-nuotraukos/bendros/LSP-koordinatoriai-2.jpg',
+        link: '/ataskaita-2022/img/koordinatoriu-nuotraukos/bendros/LSP-koordinatoriai-2.jpg',
         title: 'Coordinators-of-LSIC',
         href: '#coordinators-of-lsic'
     },
     {
-        link: '/ataskaita2022/img/koordinatoriu-nuotraukos/bendros/PKP-2.jpg',
+        link: '/ataskaita-2022/img/koordinatoriu-nuotraukos/bendros/PKP-2.jpg',
         title: 'Programs, Clubs and Projects',
         href: '#programs-clubs-and-projects'
     },
     {
-        link: '/ataskaita2022/img/koordinatoriu-nuotraukos/bendros/Uzsienieciu-koordai-2.jpg',
+        link: '/ataskaita-2022/img/koordinatoriu-nuotraukos/bendros/Uzsienieciu-koordai-2.jpg',
         title: 'Coordinators of Foreign Student Affairs',
         href: '#coordinators-of-foreign-student-affairs'
     },
@@ -80,87 +80,87 @@ export const taryba = [
     {
         name: 'Neda Žutautaitė',
         title: 'VU SA president',
-        avatar: '/ataskaita2022/img/koordinatoriu-nuotraukos/CB/Neda-Žutautaitė_Prezidentė.jpg',
+        avatar: '/ataskaita-2022/img/koordinatoriu-nuotraukos/CB/Neda-Žutautaitė_Prezidentė.jpg',
     },
     {
         name: 'Ignas Mugenis',
         title: 'VU SA CHGF chairman',
-        avatar: '/ataskaita2022/img/koordinatoriu-nuotraukos/taryba/Ignas-Mugenis_Pirmininkas_CHGF-2.jpg',
+        avatar: '/ataskaita-2022/img/koordinatoriu-nuotraukos/taryba/Ignas-Mugenis_Pirmininkas_CHGF-2.jpg',
     },
     {
-        avatar: '/ataskaita2022/img/koordinatoriu-nuotraukos/taryba/Justinas-Sacevičius_Pirmininkas_EVAF.jpg',
+        avatar: '/ataskaita-2022/img/koordinatoriu-nuotraukos/taryba/Justinas-Sacevičius_Pirmininkas_EVAF.jpg',
         name: 'Justinas Sacevičius',
         title: 'VU SA EVAF chairman',
     },
     {
-        avatar: '/ataskaita2022/img/koordinatoriu-nuotraukos/taryba/Raimondas-Šiškevičius_Pirminkas_FF.jpg',
+        avatar: '/ataskaita-2022/img/koordinatoriu-nuotraukos/taryba/Raimondas-Šiškevičius_Pirminkas_FF.jpg',
         name: 'Raimondas Šiškevičius',
         title: 'VU SA FF chairman',
     },
     {
-        avatar: '/ataskaita2022/img/koordinatoriu-nuotraukos/taryba/Elena-Pranevičiūtė_Pirmininkė_FilF-2.jpg',
+        avatar: '/ataskaita-2022/img/koordinatoriu-nuotraukos/taryba/Elena-Pranevičiūtė_Pirmininkė_FilF-2.jpg',
         name: 'Elena Pranevičiūtė',
         title: 'VU SA FilF chairwoman',
     },
     {
         name: 'Martyna Vilutytė',
         title: 'VU SA FsF chairwoman',
-        avatar: '/ataskaita2022/img/koordinatoriu-nuotraukos/taryba/Martyna-Vilutytė_Pirmininke_FsF-2.jpg',
+        avatar: '/ataskaita-2022/img/koordinatoriu-nuotraukos/taryba/Martyna-Vilutytė_Pirmininke_FsF-2.jpg',
     },
     {
         name: 'Kornelija Buivydaitė',
         title: 'VU SA GMC chairwoman',
-        avatar: '/ataskaita2022/img/koordinatoriu-nuotraukos/taryba/Kornelija-Buivydaitė_Pirmininkė_GMC-2.jpg',
+        avatar: '/ataskaita-2022/img/koordinatoriu-nuotraukos/taryba/Kornelija-Buivydaitė_Pirmininkė_GMC-2.jpg',
     },
     {
         name: 'Ieva Šiaudvytytė',
         title: 'VU SA IF chairwoman',
-        avatar: '/ataskaita2022/img/koordinatoriu-nuotraukos/taryba/Ieva-Šiaudvytytė_Pirmininke_IF-2.jpg',
+        avatar: '/ataskaita-2022/img/koordinatoriu-nuotraukos/taryba/Ieva-Šiaudvytytė_Pirmininke_IF-2.jpg',
     },
     {
         name: 'Dominyka Goldbergaitė',
         title: 'VU SA KF chairwoman',
-        avatar: '/ataskaita2022/img/koordinatoriu-nuotraukos/taryba/Dominyka-Goldbergaitė_pirmininkė_KF-2.jpg',
+        avatar: '/ataskaita-2022/img/koordinatoriu-nuotraukos/taryba/Dominyka-Goldbergaitė_pirmininkė_KF-2.jpg',
     },
     {
         name: 'Lijana Savickienė',
         title: 'VU SA KnF chairwoman',
-        avatar: '/ataskaita2022/img/koordinatoriu-nuotraukos/taryba/Lijana-Savickienė_Pirmininke_KnF.jpg',
+        avatar: '/ataskaita-2022/img/koordinatoriu-nuotraukos/taryba/Lijana-Savickienė_Pirmininke_KnF.jpg',
     },
     {
         name: 'Emilija Narušytė',
         title: 'VU SA MF chairwoman',
-        avatar: '/ataskaita2022/img/koordinatoriu-nuotraukos/taryba/Emilija-Narušytė_Pirminnke_MF-2.jpg',
+        avatar: '/ataskaita-2022/img/koordinatoriu-nuotraukos/taryba/Emilija-Narušytė_Pirminnke_MF-2.jpg',
     },
     {
         name: 'Paulina Bilinskaitė',
         title: 'VU SA MIF chairwoman',
-        avatar: '/ataskaita2022/img/koordinatoriu-nuotraukos/taryba/Paulina-Bilinskaitė_Pirminink_MIF-2.jpg',
+        avatar: '/ataskaita-2022/img/koordinatoriu-nuotraukos/taryba/Paulina-Bilinskaitė_Pirminink_MIF-2.jpg',
     },
     {
         name: 'Paulius Ladukas',
         title: 'VU SA ŠA chairman',
-        avatar: '/ataskaita2022/img/koordinatoriu-nuotraukos/taryba/Paulius-Ladukas_Pirmininkas_ŠA-2.jpg',
+        avatar: '/ataskaita-2022/img/koordinatoriu-nuotraukos/taryba/Paulius-Ladukas_Pirmininkas_ŠA-2.jpg',
     },
     {
         name: 'Airina Mikulėnaitė',
         title: 'VU SA TF chairwoman',
-        avatar: '/ataskaita2022/img/koordinatoriu-nuotraukos/taryba/Airina-Mikulėnaitė_Pirmininke_TF.jpg',
+        avatar: '/ataskaita-2022/img/koordinatoriu-nuotraukos/taryba/Airina-Mikulėnaitė_Pirmininke_TF.jpg',
     },
     {
         name: 'Ugnė Bičkauskaitė',
         title: 'VU SA TSPMI chairwoman',
-        avatar: '/ataskaita2022/img/koordinatoriu-nuotraukos/taryba/Ugnė-Bičkauskaitė_Pirmininkė_TSPMI.jpg',
+        avatar: '/ataskaita-2022/img/koordinatoriu-nuotraukos/taryba/Ugnė-Bičkauskaitė_Pirmininkė_TSPMI.jpg',
     },
     {
         name: 'Justinas Brazaitis',
         title: 'VU SA VM chairman (until December 2021)',
-        avatar: '/ataskaita2022/img/koordinatoriu-nuotraukos/taryba/Justinas-Brazaitis-–-VU-SA-VM-Pirmininkas-(iki-2021-gruodžio-mėn.).jpg',
+        avatar: '/ataskaita-2022/img/koordinatoriu-nuotraukos/taryba/Justinas-Brazaitis-–-VU-SA-VM-Pirmininkas-(iki-2021-gruodžio-mėn.).jpg',
     },
     {
         name: 'Edgaras Kodis',
         title: 'VU SA VM chairman (from 2021 gruodžio mėn.)',
-        avatar: '/ataskaita2022/img/koordinatoriu-nuotraukos/taryba/Edgaras-Kodis_Pirmininkas_VM(nuo-2021-12).jpg',
+        avatar: '/ataskaita-2022/img/koordinatoriu-nuotraukos/taryba/Edgaras-Kodis_Pirmininkas_VM(nuo-2021-12).jpg',
     },
 ]
 
--- ataskaita2022/data/friendLogos.ts
+++ ataskaita2022/data/friendLogos.ts
@@ -1,17 +1,17 @@
 const friendLogos = [
-    '/ataskaita2022/img/draugai/arqus-logotipas.png',
-    '/ataskaita2022/img/draugai/JTBA-logotipas.png',
-    '/ataskaita2022/img/draugai/LMT-logotipas.jpg',
-    '/ataskaita2022/img/draugai/Seimo-logitipas.jpg',
-    '/ataskaita2022/img/draugai/SKVC-logotipas.JPG',
-    '/ataskaita2022/img/draugai/šmsm-logotipas.jpg',
-    '/ataskaita2022/img/draugai/SPSI-logotipas.png',
-    '/ataskaita2022/img/draugai/UNICA-logotipas.png',
-    '/ataskaita2022/img/draugai/Vilnius-logotipas.png',
-    '/ataskaita2022/img/draugai/VSF-logotipas.jpg',
-    '/ataskaita2022/img/draugai/VU-logotipas.png',
-    '/ataskaita2022/img/draugai/VUB-logotipas.png',
-    '/ataskaita2022/img/draugai/ŽEF-logotipas.png'
+    '/ataskaita-2022/img/draugai/arqus-logotipas.png',
+    '/ataskaita-2022/img/draugai/JTBA-logotipas.png',
+    '/ataskaita-2022/img/draugai/LMT-logotipas.jpg',
+    '/ataskaita-2022/img/draugai/Seimo-logitipas.jpg',
+    '/ataskaita-2022/img/draugai/SKVC-logotipas.JPG',
+    '/ataskaita-2022/img/draugai/šmsm-logotipas.jpg',
+    '/ataskaita-2022/img/draugai/SPSI-logotipas.png',
+    '/ataskaita-2022/img/draugai/UNICA-logotipas.png',
+    '/ataskaita-2022/img/draugai/Vilnius-logotipas.png',
+    '/ataskaita-2022/img/draugai/VSF-logotipas.jpg',
+    '/ataskaita-2022/img/draugai/VU-logotipas.png',
+    '/ataskaita-2022/img/draugai/VUB-logotipas.png',
+    '/ataskaita-2022/img/draugai/ŽEF-logotipas.png'
 ]
 
 export default friendLogos
